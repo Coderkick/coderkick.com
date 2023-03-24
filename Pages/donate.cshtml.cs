3484835dff322a4af0ee7f3e1c469a5f388589a3
ParliamoDiPC <parliamodipc@users.noreply.github.com>
2023-03-24 22:19:38 +0100

    feat: Added donate page, but it does not hold anything since we haven't set up thedonation page yet

---
+++ Pages/donate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace coderkickcom.Pages
+{
+    public class DonateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
