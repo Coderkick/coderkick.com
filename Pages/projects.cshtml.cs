0e61be762c4a86507427791e6870b86d52a6f28f
ParliamoDiPC <parliamodipc@users.noreply.github.com>
2023-03-24 22:07:54 +0100

    feat: Added Projects page

---
+++ Pages/projects.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace coderkickcom.Pages
+{
+    public class ProjectsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
