469a79decd46bb92a2dcfd42d024679a86fe0978
ParliamoDiPC <parliamodipc@users.noreply.github.com>
2023-04-23 12:00:31 +0200

    merge: Added Debug Mode partial, fixed the GDPR one, added the Share page and edited some other, new navbar and cool stuff

---
+++ Pages/Share.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace coderkickcom.Pages
+{
+    public class ShareModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
