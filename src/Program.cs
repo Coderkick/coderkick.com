47022446a72d2364bf4843ee6bc4651d0c243f75
ParliamoDiPC <parliamodipc@users.noreply.github.com>
2023-04-23 23:26:38 +0200

    fix: removed /cdkl and /cdkl-lite redirects

--- src/Program.cs
+++ src/Program.cs
@@ -28,14 +28,6 @@ app.MapGet("/discord", () => {
     return Results.Redirect("https://discord.gg/XvQzSBRGZY");
 });
 
-app.MapGet("/cdkl", () => {
-    return Results.Redirect("/legal");
-});
-
-app.MapGet("/cdkl-lite", () => {
-    return Results.Redirect("/legal");
-});
-
 app.MapGet("/api/internal/debug.list", () => {
     String response = "";
     foreach(var dir in Directory.GetDirectories("./wwwroot/DebugExtensions"))
